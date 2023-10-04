ff6bfce2ccd3552f65352322a28a554353a3b6bf
snurnisyan <snurnisyan@gmail.com>
2023-10-04 20:43:36 +0300

    feat: first validations

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,16 +8,29 @@ import Checkbox from "../components/checkbox/chechbox.tsx";
 export default function LoginPage(): ReactElement {
 
   const [ isEmailValidated, setEmailValidation ] = useState(true);
-  let emailErrorMessage = ' ';
-  function checkValidation(e) {
-    console.log(e.target.validity.valid);
+  const [ emailError, setEmailError ] = useState (' ');
+
+  const [ isPasswordValidated, setPasswordValidation ] = useState(true);
+  const [ passwordError, setPasswordError ] = useState(' ');
+
+  const [form, setValue] = useState({ email: '', password: '' });
+
+  const isEmailEmpty = form.email.length === 0;
+  console.log(`form email: ${form.email}`);
+  const isPasswordEmpty = form.password.length === 0;
+
+  const setFormValues = e => {
+    setValue({ ...form, [e.target.name]: e.target.value });
+  };
+
+  function onFormChange(e, setValidation, setErrorMessage) {
+    setFormValues(e);
     if (!e.target.validity.valid) {
-      setEmailValidation(false);
-      console.log(e.target.validationMessage);
-      emailErrorMessage = e.target.validationMessage;
+      setValidation(false);
+      setErrorMessage(e.target.validationMessage);
     } else {
-      setEmailValidation(true);
-      let emailErrorMessage = ' ';
+      setValidation(true);
+      setErrorMessage(' ');
     }
   }
 
@@ -26,14 +39,28 @@ export default function LoginPage(): ReactElement {
 			<h2 className={'login__header'}>Войти</h2>
 			<form name="loginForm">
 				<div className={'input-container'}>
-					<input className={'input ' + (!isEmailValidated && 'input__error' || '')} type="email" name="loginEmail" required minLength="2" maxLength="40" onChange={checkValidation}></input>
-					<label className={'floating-label'}>Электронная почта</label>
-					<span className={'message ' + (!isEmailValidated && 'message__error' || '')}>{emailErrorMessage}</span>
+					<input className={'input ' + (!isEmailValidated && 'input__error' || '')}
+                 type="email"
+                 name="loginEmail"
+                 required
+                 minLength="2"
+                 maxLength="40"
+                 onChange={e => onFormChange(e, setEmailValidation, setEmailError)}>
+          </input>
+					<label className={'floating-label ' + (!isEmailEmpty && 'label-on-top' || '')}>Электронная почта</label>
+					<span className={'message ' + (!isEmailValidated && 'message__error' || '')}>{emailError}</span>
 				</div>
 				<div className={'input-container'}>
-					<input className={'input'} type="password" name="loginPassword" required minLength="6" maxLength="20"></input>
+					<input className={'input ' + (!isPasswordValidated && 'input__error' || '')}
+                 type="password"
+                 name="loginPassword"
+                 required
+                 minLength="6"
+                 maxLength="20"
+                 onChange={e => onFormChange(e, setPasswordValidation, setPasswordError)}>
+          </input>
 					<label className={'floating-label'}>Пароль</label>
-					<span className={'message message__success'}>Успех</span>
+					<span className={'message ' + (!isPasswordValidated && 'message__error' || '')}>{passwordError}</span>
 				</div>
 				<Checkbox>Запомнить меня</Checkbox>
 				<Button type={'submit'}>Войти</Button>
