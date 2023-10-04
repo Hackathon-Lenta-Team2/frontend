449b2bcdd0d123049e5a1a29c2c30ebd2e6253c6
snurnisyan <snurnisyan@gmail.com>
2023-10-04 21:54:10 +0300

    feat: 2 forms validation

--- src/components/forecast-filters/forecast-filters.tsx
+++ src/components/forecast-filters/forecast-filters.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement, useState} from 'react';
 import FilterInput from "../filter-input/filter-input.tsx";
 import Checkbox from "../checkbox/chechbox.tsx";
 import Button from "../button/button.tsx";
@@ -6,6 +6,35 @@ import "./forecast-filters.scss"
 import "../input.scss"
 
 export default function ForecastFilters(): ReactElement {
+  const [ isDaysNumberValid, setDaysNumberValidation ] = useState(true);
+  const [ daysNumberError, setDaysNumberError ] = useState ('От 1 до 14 дней');
+
+  const [form, setValue] = useState({ daysNumber: '' });
+
+  const isDaysNumberEmpty = form.daysNumber.length === 0;
+  const isFormInvalid = !isDaysNumberValid;
+
+  const setFormValues = e => {
+    setValue({ ...form, [e.target.name]: e.target.value });
+  };
+
+  function onFormChange(e) {
+    setFormValues(e);
+    if (!e.target.validity.valid) {
+      setDaysNumberValidation(false);
+      setDaysNumberError(e.target.validationMessage);
+    } else if (e.target.value > 14) {
+      setDaysNumberValidation(false);
+      setDaysNumberError('Количество дней не может быть больше 14');
+    } else if (e.target.value < 1) {
+      setDaysNumberValidation(false);
+      setDaysNumberError('Количество дней не может быть меньше 1');
+    } else {
+      setDaysNumberValidation(true);
+      setDaysNumberError('От 1 до 14 дней');
+    }
+  }
+
 	return (
 			<form name="forecast-filters" className={'form'}>
 				<FilterInput>Номер ТК</FilterInput>
@@ -14,12 +43,19 @@ export default function ForecastFilters(): ReactElement {
 				<FilterInput>Подкатегория</FilterInput>
 				<FilterInput>Товар</FilterInput>
 				<div className={'input-container number-input'}>
-					<input className={'input'} type="number" name="forecastDays" required minLength="1" maxLength="2"></input>
-					<label className={'floating-label'}>Количество дней прогноза</label>
-					<span className={'message'}> От 1 до 14 дней</span>
+					<input className={'input'}
+                 type="number"
+                 name="daysNumber"
+                 required
+                 minLength="1"
+                 maxLength="2"
+                 onChange={e => onFormChange(e)}>
+          </input>
+					<label className={'floating-label ' + (!isDaysNumberEmpty && 'label-on-top' || '')}>Количество дней прогноза</label>
+					<span className={'message ' + (!isDaysNumberValid && 'message__error' || '')}>{daysNumberError}</span>
 				</div>
 				<Checkbox>Сохранить настройки фильтров</Checkbox>
-				<Button type={'submit'}>Сформировать</Button>
+				<Button type={'submit'} disabled={isFormInvalid}>Сформировать</Button>
 			</form>
 	)
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,23 @@
 import React, {ReactElement, useState} from 'react';
 import Button from "../components/button/button.tsx";
-import './login.scss'
-import '../components/input.scss'
+import './login.scss';
+import '../components/input.scss';
 import Checkbox from "../components/checkbox/chechbox.tsx";
 
 
 export default function LoginPage(): ReactElement {
 
-  const [ isEmailValidated, setEmailValidation ] = useState(true);
+  const [ isEmailValid, setEmailValidation ] = useState(true);
   const [ emailError, setEmailError ] = useState (' ');
 
-  const [ isPasswordValidated, setPasswordValidation ] = useState(true);
+  const [ isPasswordValid, setPasswordValidation ] = useState(true);
   const [ passwordError, setPasswordError ] = useState(' ');
 
   const [form, setValue] = useState({ email: '', password: '' });
 
   const isEmailEmpty = form.email.length === 0;
-  console.log(`form email: ${form.email}`);
   const isPasswordEmpty = form.password.length === 0;
+  const isFormInvalid = !isEmailValid || !emailError;
 
   const setFormValues = e => {
     setValue({ ...form, [e.target.name]: e.target.value });
@@ -39,31 +39,31 @@ export default function LoginPage(): ReactElement {
 			<h2 className={'login__header'}>Войти</h2>
 			<form name="loginForm">
 				<div className={'input-container'}>
-					<input className={'input ' + (!isEmailValidated && 'input__error' || '')}
+					<input className={'input ' + (!isEmailValid && 'input__error' || '')}
                  type="email"
-                 name="loginEmail"
+                 name="email"
                  required
                  minLength="2"
                  maxLength="40"
                  onChange={e => onFormChange(e, setEmailValidation, setEmailError)}>
           </input>
 					<label className={'floating-label ' + (!isEmailEmpty && 'label-on-top' || '')}>Электронная почта</label>
-					<span className={'message ' + (!isEmailValidated && 'message__error' || '')}>{emailError}</span>
+					<span className={'message ' + (!isEmailValid && 'message__error' || '')}>{emailError}</span>
 				</div>
 				<div className={'input-container'}>
-					<input className={'input ' + (!isPasswordValidated && 'input__error' || '')}
+					<input className={'input ' + (!isPasswordValid && 'input__error' || '')}
                  type="password"
-                 name="loginPassword"
+                 name="password"
                  required
                  minLength="6"
                  maxLength="20"
                  onChange={e => onFormChange(e, setPasswordValidation, setPasswordError)}>
           </input>
-					<label className={'floating-label'}>Пароль</label>
-					<span className={'message ' + (!isPasswordValidated && 'message__error' || '')}>{passwordError}</span>
+					<label className={'floating-label ' + (!isPasswordEmpty && 'label-on-top' || '')}>Пароль</label>
+					<span className={'message ' + (!isPasswordValid && 'message__error' || '')}>{passwordError}</span>
 				</div>
 				<Checkbox>Запомнить меня</Checkbox>
-				<Button type={'submit'}>Войти</Button>
+				<Button type={'submit'} disabled={isFormInvalid}>Войти</Button>
 			</form>
 		</div>
 	)
