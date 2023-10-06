8f6323955d672ee74470d1f8a3ec4caeb0dcc70d
Tatiana Andreeva <snurnisyan@gmail.com>
2023-10-06 18:15:33 +0300

    feat: create non-found page

---
+++ src/pages/no-results.tsx
@@ -0,0 +1,31 @@
+import { ReactElement } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ErrorPageComponent from '../components/error-page-component/error-page-component';
+import Logo404 from '../images/404-logo.png';
+
+export default function NotFound404(): ReactElement {
+  const navigate = useNavigate();
+
+  const onClick = (): void => {
+    navigate('/');
+  };
+
+  return (
+    <ErrorPageComponent
+      headerText='Страница не найдена'
+  buttonText='Главная'
+  logo={Logo404}
+  onClick={onClick}
+  >
+  <p style={{ margin: 0 }}>
+  Страница, которую вы запрашиваете, не существует.
+  </p>
+  <p style={{ margin: 0 }}>
+  Возможно, она была удалена, или вы набрали неверный адрес.
+  </p>
+  <p style={{ margin: 0 }}>
+  Всю информацию можно получить на Главной странице
+  </p>
+  </ErrorPageComponent>
+);
+}
--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,5 +1,7 @@
-import {ReactElement} from 'react';
-import {useNavigate} from 'react-router-dom';
+import { ReactElement } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ErrorPageComponent from '../components/error-page-component/error-page-component';
+import Logo404 from '../images/404-logo.png';
 
 export default function NotFound404(): ReactElement {
   const navigate = useNavigate();
@@ -9,12 +11,21 @@ export default function NotFound404(): ReactElement {
   };
 
   return (
-    <div>
-      <h1>Упс! Ошибка 404</h1>
-      <p>Страница, которую вы запрашиваете, не существует :(</p>
-      <button type="button" onClick={onClick}>
-        Вернуться на главную
-      </button>
-    </div>
+    <ErrorPageComponent
+      headerText='Страница не найдена'
+      buttonText='Главная'
+      logo={Logo404}
+      onClick={onClick}
+    >
+      <p style={{ margin: 0 }}>
+        Страница, которую вы запрашиваете, не существует.
+      </p>
+      <p style={{ margin: 0 }}>
+        Возможно, она была удалена, или вы набрали неверный адрес.
+      </p>
+      <p style={{ margin: 0 }}>
+        Всю информацию можно получить на Главной странице
+      </p>
+    </ErrorPageComponent>
   );
 }
