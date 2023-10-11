0295fb99a141b6353b1fd62ea97a28a19b1aa7d5
Tatiana Andreeva <snurnisyan@gmail.com>
2023-10-11 11:19:00 +0300

    fix: auth fix for demo

--- src/components/routes-component/routes-component.tsx
+++ src/components/routes-component/routes-component.tsx
@@ -5,26 +5,18 @@ import LoginPage from '../../pages/login';
 import FilterPage from '../../pages/filter';
 import TablePage from '../../pages/table/TablePage';
 import NoResults from '../../pages/no-results';
-import ProtectedRoute from '../protected-route/protected-route';
 import DashboardPage from '../../pages/dashboard/DashboardPage';
 
 export default function RoutesComponent(): ReactElement {
   return (
     <Routes>
-      <Route path='/' element={<ProtectedRoute element={<FilterPage />} />} />
+      {/*  часть маршрутов - защищенная, для демо убрали защиту
+  <Route path='/' element={<ProtectedRoute element={<FilterPage />} />} /> */}
       <Route path='/login' element={<LoginPage />} />
-      <Route
-        path='/results/table/:dataType'
-        element={<ProtectedRoute element={<TablePage />} />}
-      />
-      <Route
-        path='/results/dashboard/:dataType'
-        element={<ProtectedRoute element={<DashboardPage />} />}
-      />
-      <Route
-        path='/no-results'
-        element={<ProtectedRoute element={<NoResults />} />}
-      />
+      <Route path='/' element={<FilterPage />} />
+      <Route path='/results/table/:dataType' element={<TablePage />} />
+      <Route path='/results/dashboard/:dataType' element={<DashboardPage />} />
+      <Route path='/no-results' element={<NoResults />} />
       <Route path='*' element={<NotFound404 />} />
     </Routes>
   );
